b2f1dd6474c19df9aa51e978e6fc583b0ca651c2
kerchner <EAD\kerchner@gwul-fb-vm001.ead.gwu.edu>
2014-04-25 12:07:22 -0400

    Added SensorResource, querysets for both resources

--- hotbot/ui/api.py
+++ hotbot/ui/api.py
@@ -1,12 +1,30 @@
+from tastypie import fields
 from tastypie.authentication import ApiKeyAuthentication, \
     MultiAuthentication, Authentication
 from tastypie.authorization import DjangoAuthorization
+from tastypie.constants import ALL_WITH_RELATIONS
 from tastypie.resources import ModelResource
 from ui.models import Sensor, Reading
 
 
-class ReadingResource(ModelResource):
+class SensorResource(ModelResource):
     class Meta:
+        queryset = Sensor.objects.all()
+	filtering = {
+            'macaddr': 'exact',
+        }
+        authentication = MultiAuthentication(ApiKeyAuthentication(),
+                                             Authentication())
+        authorization = DjangoAuthorization()
+
+
+class ReadingResource(ModelResource):
+    sensor = fields.ForeignKey(SensorResource, 'sensor')
+    class Meta:
+	queryset = Reading.objects.all()
+        filtering = {
+            'sensor': ALL_WITH_RELATIONS
+        }
 	authentication = MultiAuthentication(ApiKeyAuthentication(),
                                              Authentication())
         authorization = DjangoAuthorization()
