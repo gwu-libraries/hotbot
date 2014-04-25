1ac184741f480dcc8626a0feec5d101e5979d5a6
kerchner <EAD\kerchner@gwul-fb-vm001.ead.gwu.edu>
2014-04-25 10:10:27 -0400

    Initial model

--- hotbot/ui/models.py
+++ hotbot/ui/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Sensor(models.Model):
+    macaddr = models.CharField(max_length=12, db_index=True)
+    name = models.CharField(max_length=50, db_index=True)
+
+
+class Reading(models.Model):
+    sensor = models.ForeignKey(Sensor, related_name='sensors')
+    metric = models.CharField(max_length=20, db_index=True)
+    value = models.FloatField()
