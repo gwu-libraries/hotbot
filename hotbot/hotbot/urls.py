0719f0c860e29c36a3091b4f1f5e66500e75932f
kerchner <EAD\kerchner@gwul-fb-vm001.ead.gwu.edu>
2014-04-25 10:59:30 -0400

    Added api url

--- hotbot/hotbot/urls.py
+++ hotbot/hotbot/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from tastypie.api import Api
+from ui.api import *
 admin.autodiscover()
+
+v1_api = Api(api_name='v1')
+v1_api.register(ReadingResource())
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,4 +13,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(v1_api.urls)),
 )
