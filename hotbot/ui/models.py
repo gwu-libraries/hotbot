5d756c14df6e913b45e780975a2ec748141b4128
kerchner <EAD\kerchner@gwul-fb-vm001.ead.gwu.edu>
2014-04-25 12:08:32 -0400

    added unicode method for both classes

--- hotbot/ui/models.py
+++ hotbot/ui/models.py
@@ -11,9 +11,15 @@ class Sensor(models.Model):
     macaddr = models.CharField(max_length=12, db_index=True, primary_key=True)
     name = models.CharField(max_length=50, db_index=True, blank=True)
 
+    def __unicode__(self):
+        return self.macaddr
+
 
 class Reading(models.Model):
     sensor = models.ForeignKey(Sensor, related_name='sensors')
     metric = models.CharField(max_length=20, db_index=True)
     value = models.FloatField()
     timestamp = models.DateTimeField(db_index=True, default=now)
+
+    def __unicode__(self):
+        return self.id
