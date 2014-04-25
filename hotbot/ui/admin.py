b90162ff2fc66470e7bff9ccd40a833acd7db40e
Darshan Pandhi <darshan@email.gwu.edu>
2014-04-25 10:32:11 -0400

    Added admin models

--- hotbot/ui/admin.py
+++ hotbot/ui/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from ui.models import Sensor, Reading
+from tastypie.admin import ApiKeyInline
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 # Register your models here.
+
+class UserModelAdmin(UserAdmin):
+    inlines = UserAdmin.inlines + [ApiKeyInline]
+
+class SensorAdmin(admin.ModelAdmin):
+    fields = ['macaddr', 'name']
+    list_display = ('macaddr', 'name')
+    search_fields = ['macaddr', 'name']
+
+class ReadingAdmin(admin.ModelAdmin):
+    fields = ['sensor', 'metric', 'value', 'timestamp']
+    list_display = ('sensor', 'metric', 'value', 'timestamp')
+    search_fields = ['sensor']
+    date_hierarchy = 'timestamp'
+
+admin.site.unregister(User)
+admin.site.register(User, UserModelAdmin)
+admin.site.register(Sensor, SensorModelAdmin)
+admin.site.register(Reading, ReadingAdmin)
